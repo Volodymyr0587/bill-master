82c82acb3377551ff98f9642784726881140b15d
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-24 18:47:25 +0300

    Layout ...

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bill master</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="m-0 bg-center bg-contain">
+    <!-- Header Section -->
+    <header>
+        <div class="text-4xl ml-6">
+              Bill Master
+          </div>
+        <div class="text-xl ml-10 mb-16 m1-4">
+            Simplifying Your Utility Payments
+          </div>
+    </header>
+
+    <!-- Menu Navigation Bar -->
+    <nav class="sticky bg-lime-300 py-4 text-green-900 overflow-hidden">
+        <a class="float-left text-center text-lg px-4 py-4 hover:text-green-500" href="#home">HOME</a>
+        <a class="float-left text-center text-lg px-4 py-4 hover:text-green-500" href="#news">NEWS</a>
+        <a class="float-left text-center text-lg px-4 py-4 hover:text-green-500" href="#notification">NOTIFICATIONS</a>
+        <div class="float-right right-auto text-center text-lg px-4 py-4 hover:text-green-500">
+            <a href="#login">REGISTER</a>
+        </div>
+        <div class="float-right right-auto text-center text-lg px-4 py-4 hover:text-green-500">
+            <a href="#login">LOGIN</a>
+        </div>
+    </nav>
+
+    <!-- Body section -->
+    <main class="ml-20 mt-10">
+
+            <h3>Content section</h3>
+
+    </main>
+
+    <!-- Footer Section -->
+    <footer class="w-full bottom-0 bg-black text-white absolute pt-6 text-center text-lg font-bold">
+        Footer Section
+    </footer>
+</body>
+</html>
