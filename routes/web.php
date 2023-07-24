80efdcd1bab01fbe516e9fe68216e44507ecac95
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-24 18:48:59 +0300

    Layout 2

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('layouts.app');
 });
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
