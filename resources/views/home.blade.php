754ef4006c9d111c0f13107891f3abe368e790ef
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-21 17:08:49 +0200

    Responsive table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex justify-center">
+    {{-- <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
             Home
         </div>
+        <div>
+            <a href="{{ route('electricity') }}">Electricty</a>
+        </div>
 
-        <a href="{{ route('electricity') }}">Electricty</a>
-    </div>
+
+    </div> --}}
+
+    <x-table />
 @endsection
